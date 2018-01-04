8c63081060c3144b311726f98d158318d33ffadd
Andrei Gubceac <andrei.gubceac@me.com>
2018-01-04 23:02:35 +0200

    Started with new project.

---
+++ AGCloudKit/AGCloudKit/AGCloudKit.h
@@ -0,0 +1,19 @@
+//
+//  AGCloudKit.h
+//  AGCloudKit
+//
+//  Created by Andrei Gubceac on 04/01/2018.
+//  Copyright © 2018 Andrei Gubceac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGCloudKit.
+FOUNDATION_EXPORT double AGCloudKitVersionNumber;
+
+//! Project version string for AGCloudKit.
+FOUNDATION_EXPORT const unsigned char AGCloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGCloudKit/PublicHeader.h>
+
+
